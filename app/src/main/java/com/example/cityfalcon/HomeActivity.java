c9fb6590e7da6483414ef24c56f58a47d9462b50
вадим дудиев <vdudiev@inbox.ru>
2020-05-22 03:01:13 +0300

    first commit

---
+++ app/src/main/java/com/example/cityfalcon/HomeActivity.java
@@ -0,0 +1,59 @@
+package com.example.cityfalcon;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class HomeActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        BottomNavigationView navigationView = findViewById(R.id.bottom_navigation);
+        navigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                int id = menuItem.getItemId();
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                switch (id) {
+                    case R.id.nav_signal:
+                        SignalFragment fragmentSignal = new SignalFragment();
+                        fragmentTransaction.replace(R.id.content_fragment,fragmentSignal);
+                        break;
+
+                    case R.id.nav_watchlist:
+                        WatchlistFragment fragmentWatchlist = new WatchlistFragment();
+                        fragmentTransaction.replace(R.id.content_fragment,fragmentWatchlist);
+                        break;
+
+                    case R.id.nav_search:
+                        SearchFragment fragmentSearch = new SearchFragment();
+                        fragmentTransaction.replace(R.id.content_fragment,fragmentSearch);
+                        break;
+
+                    case R.id.nav_sectors:
+                        SectorsFragment fragmentSectors = new SectorsFragment();
+                        fragmentTransaction.replace(R.id.content_fragment,fragmentSectors);
+                        break;
+
+                    case R.id.nav_more:
+                        MoreFragment fragmentMore = new MoreFragment();
+                        fragmentTransaction.replace(R.id.content_fragment,fragmentMore);
+                        break;
+                }
+                fragmentTransaction.commit();
+                return true;
+            }
+        });
+
+    }
+}
+
+
